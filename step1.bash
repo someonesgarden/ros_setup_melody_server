dc49c56155960168c0637e279c145813895ebe49
someonesgarden <d@someonesgarden.org>
2021-01-17 13:24:55 +0000

    fixed

--- step1.bash
+++ step1.bash
@@ -14,21 +14,22 @@ while ! sudo apt-get install -y curl ; do
 done
 set -vx
 
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 
 
 curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
-sudo apt update || echo ""
+sudo apt-get update || echo ""
 
-sudo apt install -y ros-${ROS_VER}-ros-base
+sudo apt-get install -y ros-${ROS_VER}-ros-base
 
 ls /etc/ros/rosdep/sources.list.d/20-default.list && sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
 
-sudo apt install -y python-pip
+sudo apt-get install -y python-pip
 
 
-sudo pip install -U -y rosdep
+sudo pip-get install -U -y rosdep
 
 sudo rosdep init 
 rosdep update
