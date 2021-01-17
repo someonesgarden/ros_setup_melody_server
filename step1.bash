862b0c3b05cf38850a5d5f05d194f15c4f87050f
someonesgarden <d@someonesgarden.org>
2021-01-17 13:15:44 +0000

    fixed

--- step1.bash
+++ step1.bash
@@ -14,6 +14,10 @@ while ! sudo apt-get install -y curl ; do
 done
 set -vx
 
+
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+
+
 curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
 sudo apt-get update || echo ""
 
